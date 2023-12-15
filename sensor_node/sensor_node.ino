3faa1c59fd049d907fef3a342bfb99aa48327806
Nguyễn Phúc Tiến <nguyenphuctien1258@gmail.com>
2023-12-15 13:47:39 +0700

    fix cast type

--- sensor_node/sensor_node.ino
+++ sensor_node/sensor_node.ino
@@ -29,44 +29,43 @@ struct other_sensor
 int size_dht20_struct = sizeof(struct dht20);
 int size_other_struct = sizeof(struct other_sensor);
 
-bool first;
-
-struct Gyro_data_structure dht20_data = {"light", "15", "23", "0x123123"};
-struct Gyro_data_structure light_sensor_data = {"light", "31", "55", "0x1234123"};
-struct Gyro_data_structure soil_mois_data = {"soil_moistuser", "31", "55", "0x1234123"};
+struct dht20 dht20_data = {"light", "15", "23", "0x123123"};
+struct other_sensor light_sensor_data = {"light", "31", "0x1234123"};
+struct other_sensor soil_mois_data = {"soil_moistuser", "31", "0x1234123"};
 
 int status = DHT20;
+
 void setup()
 {
-  Serial.begin(115200);    
-  status = DHT20;
+    Serial.begin(115200);
+    status = DHT20;
 }
 
 void loop()
 {
-  switch (status) {
-    case DHT20:
-      Serial.write('<'); // Start of frame
-      Serial.write((const char *)dht20_data, _size);
-      Serial.write('#');  // end of frame
-      status = LIGHT_SENSOR;
-      break;
-    case LIGHT_SENSOR:
-        Serial.write('<'); 
-        Serial.write((const char *)light_sensor_data, _size);
-        Serial.write('#');  // end of frame
-        status = SOIL_MOIS;
-        break;
-    case SOIL_MOIS:
-        Serial.write('<'); // Start of frame
-        Serial.write((const char *)soil_mois_data, _size);
-        Serial.write('#');  // end of frame      
-        status = DHT20;
-        break;
-    default:
-      break;
-  }
+    switch (status) {
+        case DHT20:
+            Serial.write('<'); // Start of frame
+            Serial.write((const char *)&dht20_data, size_dht20_struct);
+            Serial.write('#');  // end of frame
+            status = LIGHT_SENSOR;
+            break;
+        case LIGHT_SENSOR:
+            Serial.write('<');
+            Serial.write((const char *)&light_sensor_data, size_other_struct);
+            Serial.write('#');  // end of frame
+            status = SOIL_MOIS;
+            break;
+        case SOIL_MOIS:
+            Serial.write('<'); // Start of frame
+            Serial.write((const char *)&soil_mois_data, size_other_struct);
+            Serial.write('#');  // end of frame
+            status = DHT20;
+            break;
+        default:
+            break;
+    }
 
-  Serial.println();
-  delay(SEND_TIME);
+    Serial.println();
+    delay(SEND_TIME);
 }
